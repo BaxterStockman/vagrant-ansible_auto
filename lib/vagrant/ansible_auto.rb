bead708b0a6dbf72165e09dc9f92e5aed18d1014
Matt Schreiber <EMAIL>
2017-07-21 16:27:29 -0400

    Add source_root method

--- lib/vagrant/ansible_auto.rb
+++ lib/vagrant/ansible_auto.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
+
+require 'pathname'
+
 require 'vagrant'
 
 require 'vagrant/ansible_auto/plugin'
 require 'vagrant/ansible_auto/version'
+
+module VagrantPlugins
+  # Ansible command and provisioner plugin with automatic inventory generation
+  module AnsibleAuto
+    class << self
+      # @return [Pathname] path to the gem source root directory
+      def source_root
+        Pathname.new('../../..').expand_path(__FILE__)
+      end
+    end
+  end
+end

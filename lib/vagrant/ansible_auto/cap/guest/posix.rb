7cc245011d2444af21f7359bb20453a8023decf6
Matt Schreiber <EMAIL>
2017-07-26 14:56:53 -0400

    Add POSIX capability parent module for documentation purposes

---
+++ lib/vagrant/ansible_auto/cap/guest/posix.rb
@@ -0,0 +1,16 @@
+
+# Here for documentation purposes only
+
+module VagrantPlugins
+  module AnsibleAuto
+    # Capabilities on guests and hosts
+    module Cap
+      # Capabilities on guests
+      module Guest
+        # Capabilities on POSIX hosts
+        module POSIX
+        end
+      end
+    end
+  end
+end

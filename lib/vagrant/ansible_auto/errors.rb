ef6fa5717c79a33f6430230f3b1a13fd46f06d66
Matt Schreiber <EMAIL>
2017-07-26 14:07:49 -0400

    Add GroupMissingChildError and InvalidHostTypeError classes and add docstrings to all error classes

--- lib/vagrant/ansible_auto/errors.rb
+++ lib/vagrant/ansible_auto/errors.rb
@@ -1,13 +1,27 @@
 # frozen_string_literal: true
+
 module VagrantPlugins
   module AnsibleAuto
+    # Error classes for the +ansible_auto+ provisioner and +ansible+ command
     module Errors
+      # An error representing an {Inventory} misconfiguration
       class InventoryError < Vagrant::Errors::VagrantError
-        error_namespace('vagrant.provisioners.ansible_auto')
+        error_namespace('vagrant.ansible_auto.errors.inventory')
       end
 
+      # Raised when an Ansible inventory group is expected to exist but doesn't
       class MissingGroupError < InventoryError
         error_key(:missing_group)
+      end
+
+      # Raised when a group specifies child groups that do not exist
+      class GroupMissingChildError < MissingGroupError
+        error_key(:group_missing_child)
+      end
+
+      # Raised when provided data can't be converted to a {Host} object
+      class InvalidHostTypeError < InventoryError
+        error_key(:invalid_host_type)
       end
     end
   end

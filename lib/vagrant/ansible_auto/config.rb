ae1b18158e4357ba1f9a444f321a9345136d665a
Matt Schreiber <EMAIL>
2017-07-26 19:50:25 -0400

    Add insert_control_machine_public_key and upload_inventory_host_private_keys options and YARD documentation

--- lib/vagrant/ansible_auto/config.rb
+++ lib/vagrant/ansible_auto/config.rb
@@ -1,56 +1,82 @@
 # frozen_string_literal: true
-require 'vagrant/ansible_auto/inventory'
-require 'vagrant/ansible_auto/errors'
-require 'vagrant/util/deep_merge'
 
 require 'vagrant'
 require Vagrant.source_root + 'plugins/provisioners/ansible/config/guest'
 
+require 'vagrant/ansible_auto/inventory'
+require 'vagrant/ansible_auto/errors'
+require 'vagrant/ansible_auto/util/config'
+
 module VagrantPlugins
   module AnsibleAuto
+    # Configuration for the +ansible_auto+ provisioner and +ansible+ command
     class Config < VagrantPlugins::Ansible::Config::Guest
+      include Util::Config
+
       attr_accessor :inventory, :groups, :vars, :children,
-                    :strict_host_key_checking, :host_connect_tries, :host_connect_sleep
+                    :strict_host_key_checking, :host_connect_tries,
+                    :host_connect_sleep, :insert_control_machine_public_key,
+                    :upload_inventory_host_private_keys
 
       protected(:inventory=)
 
       def initialize
         super
-        @inventory                = Inventory.new
-        @groups                   = UNSET_VALUE
-        @vars                     = UNSET_VALUE
-        @children                 = UNSET_VALUE
-        @strict_host_key_checking = UNSET_VALUE
-        @host_connect_tries       = UNSET_VALUE
-        @host_connect_sleep       = UNSET_VALUE
-        @__errors                 = []
+        @inventory                          = Inventory.new
+        @groups                             = UNSET_VALUE
+        @vars                               = UNSET_VALUE
+        @children                           = UNSET_VALUE
+        @strict_host_key_checking           = UNSET_VALUE
+        @host_connect_tries                 = UNSET_VALUE
+        @host_connect_sleep                 = UNSET_VALUE
+        @insert_control_machine_public_key  = UNSET_VALUE
+        @upload_inventory_host_private_keys = UNSET_VALUE
+        @__errors = []
       end
 
+      # Set default configuration values at the end of constructing the Vagrant
+      # environment
+      # @api private
+      # @return [void]
       def finalize!
         super
-        @inventory.groups         = @groups   unless @groups                == UNSET_VALUE
-        @inventory.vars           = @vars     unless @vars                  == UNSET_VALUE
-        @inventory.children       = @children unless @children              == UNSET_VALUE
-        @strict_host_key_checking = false     if @strict_host_key_checking  == UNSET_VALUE
-        @host_connect_tries       = 10        if @host_connect_tries        == UNSET_VALUE
-        @host_connect_sleep       = 2         if @host_connect_sleep        == UNSET_VALUE
+        @inventory.groups                   = @groups                                 unless @groups                          == UNSET_VALUE
+        @inventory.vars                     = @vars                                   unless @vars                            == UNSET_VALUE
+        @inventory.children                 = @children                               unless @children                        == UNSET_VALUE
+        @strict_host_key_checking           = false                                   if @strict_host_key_checking            == UNSET_VALUE
+        @host_connect_tries                 = 10                                      if @host_connect_tries                  == UNSET_VALUE
+        @host_connect_sleep                 = 2                                       if @host_connect_sleep                  == UNSET_VALUE
+        @insert_control_machine_public_key  = true                                    if @insert_control_machine_public_key   == UNSET_VALUE
+        @upload_inventory_host_private_keys = !@insert_control_machine_public_key     if @upload_inventory_host_private_keys  == UNSET_VALUE
+
+        # NOTE this is defined in core Vagrant's Ansible config.
+        @limit = '*' if @limit == UNSET_VALUE
       rescue Errors::InventoryError => e
         @__errors << e.message
       end
 
+      # Ensure that the configuration is in a valid state
+      # @api private
+      # @return [Hash{String=>Array}] a structure containing a list of errors
+      #   under the +ansible_auto+ key
       def validate(machine)
         super
 
         errors = _detected_errors + @__errors
 
         # TODO: -- test that `host_wait...' values are integers
-        unless @strict_host_key_checking == true or @strict_host_key_checking == false
-          errors << "strict_host_key_checking must be either true or false"
+        unless @strict_host_key_checking == true || @strict_host_key_checking == false
+          errors << 'strict_host_key_checking must be either true or false'
         end
 
         { 'ansible_auto' => errors }
       end
 
+      # Merge two configurations
+      # @api private
+      # @param [Config] other the configuration to merge into this
+      #   configuration
+      # @return [Config] the merged configuration
       def merge(other)
         return super if other.nil?
 
